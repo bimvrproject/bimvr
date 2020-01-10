a5f547dca9b5f2317a8128ab90142650b813dbff
wangshuihu <wangshuihu7@163.com>
2020-01-10 17:43:41 +0800

    denglu

--- src/main/java/com/jhbim/bimvr/controller/pc/community/RankingController.java
+++ src/main/java/com/jhbim/bimvr/controller/pc/community/RankingController.java
@@ -8,6 +8,7 @@ import com.jhbim.bimvr.system.enums.ResultStatusCode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,7 +20,7 @@ import java.util.List;
  * @create 2020-01-10 13:17
  */
 @RestController
-//@RequestMapping("/${version}/ranking")
+@RequestMapping("/${version}/ranking")
 public class RankingController {
     private int number=1;
     @Autowired
