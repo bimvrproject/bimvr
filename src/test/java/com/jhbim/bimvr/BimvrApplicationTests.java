764d8b16513a293fe6196a055120bf47e852d582
211005788@qq.com <chenshuaixi0>
2020-03-18 11:03:02 +0800

    2020-3-18 11:02 恢复到3-18 9:47的版本

--- src/test/java/com/jhbim/bimvr/BimvrApplicationTests.java
+++ src/test/java/com/jhbim/bimvr/BimvrApplicationTests.java
@@ -15,67 +15,10 @@ import java.io.IOException;
 @SpringBootTest
 public class BimvrApplicationTests {
 
-
-
     @Test
-    public void contextLoads() throws IOException {
+    public void contextLoads(){
 
-//        FTPClient ftpClient = new FTPClient();
-//        ftpClient.connect("36.112.65.110", 21);
-//        ftpClient.login("Administrator", "Jhkj73630915");
-//        ftpClient.changeWorkingDirectory("/");
-//        ftpClient.rmd("/12/");
-//        if (ftpClient != null && ftpClient.isConnected()) {
-//            ftpClient.logout();
-//            ftpClient.disconnect();
-//            System.out.println("FTP服务器关闭连接！");
-//        }
-//        aa();
     }
 
-    public void aa(){
-        FTPClient ftpClient = new FTPClient();
-        try {
-            ftpClient.connect("36.112.65.110", 21);
-            ftpClient.login("Administrator", "Jhkj73630915");
-            int reply = ftpClient.getReplyCode();
-            if (!FTPReply.isPositiveCompletion(reply)) {
-                System.out.println("FTP server refused connection");
-                ftpClient.disconnect();
-            }
-            // 切换到根目录
-            ftpClient.changeWorkingDirectory("/TomcatRoot/project/");
-            int aa=7;
-            String path = aa+"/999/week2/GD/UPLOAD";
-            String[] pah = path.split("/");
-            // 分层创建目录
-            for (String pa : pah) {
-                System.out.println(pa);
-                ftpClient.makeDirectory(pa);
-                // 切到到对应目录
-                ftpClient.changeWorkingDirectory(pa);
-            }
 
-
-
-
-
-            String path1 = aa+"/999/week1/GD/UPLOAD";
-            String[] pah1 = path1.split("/");
-            // 分层创建目录
-            for (String pa1 : pah1) {
-                System.out.println(pa1);
-                ftpClient.makeDirectory(pa1);
-                // 切到到对应目录
-                ftpClient.changeWorkingDirectory(pa1);
-            }
-            if (ftpClient != null && ftpClient.isConnected()) {
-                ftpClient.logout();
-                ftpClient.disconnect();
-                System.out.println("FTP服务器关闭连接！");
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
