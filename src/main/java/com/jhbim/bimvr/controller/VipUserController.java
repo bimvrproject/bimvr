f03f0ce75c66fbf27f717dee71b53c586cc31b3b
211005788@qq.com <chenshuaixi0>
2020-04-01 10:31:37 +0800

    2020-04-01 关闭浏览器窗口（删除）

--- src/main/java/com/jhbim/bimvr/controller/VipUserController.java
+++ src/main/java/com/jhbim/bimvr/controller/VipUserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/Vipuser")
@@ -19,19 +18,5 @@ public class VipUserController {
     @RequestMapping("/selectvip")
     public Result selectvip(String phone){
         return new Result(ResultStatusCode.OK,userMapper.selectByPrimaryKey(phone));
-    }
-
-    @RequestMapping("/close")
-    public void close(){
-        try {
-            Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
-            Runtime.getRuntime().exec("taskkill /F /IM iexplorer.exe");
-            Runtime.getRuntime().exec("taskkill /F /IM firefox.exe");
-            Runtime.getRuntime().exec("taskkill /F /IM safari.exe");
-            Runtime.getRuntime().exec("taskkill /F /IM opera.exe");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 }
